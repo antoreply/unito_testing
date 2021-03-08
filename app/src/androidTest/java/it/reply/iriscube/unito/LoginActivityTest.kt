2bd8bb6cd3b2c9f763594fcbbb7f157e8c88b5e3
m.livraga <m.livraga@reply.it>
2021-03-08 18:55:02 +0100

    added exercise

--- app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
+++ app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
@@ -1,12 +1,11 @@
 package it.reply.iriscube.unito
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import it.reply.iriscube.unito.activity.LoginActivity
-import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 
@@ -21,7 +20,55 @@ class LoginActivityTest {
 
     @Test
     fun verifyFieldValidation() {
-        onView(withId(R.id.userNameTextView)).check(matches(not(withText(""))))
-        onView(withId(R.id.passwordTextView)).check(matches(not(withText(""))))
+        onView(withId(R.id.userNameTextView))
+            .check(matches(withText("")))
+        onView(withId(R.id.passwordTextView))
+            .check(matches(withText("")))
     }
+
+    @Test
+    fun verifyErrorLabelsDisplayed(){
+        onView(withId(R.id.loginButton))
+            .perform(click())
+
+        onView(withId(R.id.userNameErrorView))
+            .check(matches(isDisplayed()))
+
+        onView(withId(R.id.passwordErrorView))
+            .check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun verifyInputUsername(){
+        val username = "MarioRossi123"
+
+        onView(withId(R.id.userNameTextView))
+            .perform(typeText(username), closeSoftKeyboard())
+
+        onView(withId(R.id.passwordTextView))
+            .perform(typeText("LaMIaPassWOrd"), closeSoftKeyboard())
+
+        onView(withId(R.id.loginButton))
+            .perform(click())
+
+        onView(withId(R.id.nameTxt))
+            .check(matches(withText(username)))
+
+    }
+
+    @Test
+    fun verifyEmailDetail(){
+
+        // TODO navigate to the detail view, inside "il mio profilo", and check the shown mail is "m.rossi@reply.it"
+
+    }
+
+    @Test
+    fun verifyBirthPlace(){
+
+        // TODO navigate to the detail view, inside "il mio profilo", and check the birth place is "Torino"
+
+    }
+
 }
