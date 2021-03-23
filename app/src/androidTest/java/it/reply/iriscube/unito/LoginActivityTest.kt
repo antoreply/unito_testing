7f41962c1788ece5c48d661daef55fe9874fa815
m.livraga <m.livraga@reply.it>
2021-03-23 18:56:05 +0100

    updated test

--- app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
+++ app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
@@ -3,20 +3,20 @@ package it.reply.iriscube.unito
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import it.reply.iriscube.unito.activity.LoginActivity
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-/**
- * Created by Reply.
- */
-
+@RunWith(AndroidJUnit4::class)
 class LoginActivityTest {
 
     @get:Rule
-     val mActivityTestRule = ActivityTestRule<LoginActivity>(LoginActivity::class.java)
+     val activityRule = ActivityScenarioRule(LoginActivity::class.java)
 
     @Test
     fun verifyLayoutLabelContent() {
@@ -28,98 +28,41 @@ class LoginActivityTest {
 
         onView(withId(R.id.loginButton))
             .check(matches(withText(R.string.entra)))
-
     }
 
     @Test
-    fun verifyErrorLabelsDisplayed(){
-        onView(withId(R.id.userNameTextView))
-            .perform(typeText(""), closeSoftKeyboard())
+    fun showErrorViewsOnWrongCredentials(){
+        onView(withId(R.id.wrongCredential))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
 
+        onView(withId(R.id.userNameTextView))
+            .perform(typeText("username"), closeSoftKeyboard())
         onView(withId(R.id.passwordTextView))
-            .perform(typeText(""), closeSoftKeyboard())
+            .perform(typeText("password"), closeSoftKeyboard())
 
         onView(withId(R.id.loginButton))
             .perform(click())
 
+        onView(withId(R.id.wrongCredential))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+    }
+
+    @Test
+    fun showErrorsViewOnEmptyCredentials(){
         onView(withId(R.id.userNameErrorView))
-            .check(matches(isDisplayed()))
-
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
         onView(withId(R.id.passwordErrorView))
-            .check(matches(isDisplayed()))
-
-    }
-
-    @Test
-    fun verifyInputUsername(){
-        val username = "MarioRossi123"
-
-        onView(withId(R.id.userNameTextView))
-            .perform(typeText(username), closeSoftKeyboard())
-
-        onView(withId(R.id.passwordTextView))
-            .perform(typeText("LaMIaPassWOrd"), closeSoftKeyboard())
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)))
 
         onView(withId(R.id.loginButton))
             .perform(click())
 
-        onView(withId(R.id.nameTxt))
-            .check(matches(withText(username)))
-
-    }
-
-    @Test
-    fun verifyEmailDetail(){
-
-        gotoDetails()
-
-        onView(withId(R.id.profileEmailTxt))
-            .check(matches(withText("m.rossi@reply.it")))
-
-    }
-
-    @Test
-    fun verifyBirthPlace(){
-
-        gotoDetails()
-
-        onView((withId(R.id.profileBornCityTxt)))
-            .check(matches(withText("Torino")))
-
-    }
-
-   @Test
-   fun verifyLoginCredentialRetain(){
-
-       val username = "myPublicUsername"
-
-       onView(withId(R.id.userNameTextView))
-           .perform(typeText(username), closeSoftKeyboard())
-       onView((withId(R.id.passwordTextView)))
-           .perform(typeText("test"), closeSoftKeyboard())
-
-       onView(withId(R.id.loginButton))
-           .perform(click())
-       onView(withId(R.id.logoutIcn))
-           .perform(click())
-
-       onView(withId(R.id.userNameTextView))
-           .check(matches(withText(username)))
-
-   }
-
-    private fun gotoDetails(){
         onView(withId(R.id.userNameTextView))
-            .perform(typeText("test"), closeSoftKeyboard())
-        onView(withId(R.id.passwordTextView))
-            .perform(typeText("test"), closeSoftKeyboard())
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
+        onView(withId(R.id.passwordErrorView))
+            .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)))
 
-        onView(withId(R.id.loginButton))
-            .perform(click())
-        onView(withId(R.id.nextIcn))
-            .perform(click())
-        onView(withId(R.id.profileContainer))
-            .perform(click())
     }
+
 
 }
---
+++ app/src/androidTest/java/it/reply/iriscube/unito/ProfileActivityTest.kt
@@ -0,0 +1,41 @@
+package it.reply.iriscube.unito
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import it.reply.iriscube.unito.activity.ProfileActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ProfileActivityTest {
+
+    @Test
+    fun verifyNoEmptyFields(){
+        ActivityScenario.launch(ProfileActivity::class.java)
+
+        onView(withId(R.id.profileNameTxt))
+            .check(matches(withText("Mario")))
+
+        onView(withId(R.id.profileSurnameTxt))
+            .check(matches(withText("Rossi")))
+
+        onView(withId(R.id.profileEmailTxt))
+            .check(matches(withText("m.rossi@reply.it")))
+
+    }
+
+    @Test
+    fun verifyBirthPlaceDisplayed(){
+        ActivityScenario.launch(ProfileActivity::class.java)
+
+        onView(withId(R.id.profileBornCityTxt))
+            .check(matches(isDisplayed()))
+
+    }
+
+}
