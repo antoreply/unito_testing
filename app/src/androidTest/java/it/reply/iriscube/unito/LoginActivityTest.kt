783dc4c029e92c9d308c842360526b7d935084a4
m.livraga <m.livraga@reply.it>
2021-03-23 16:17:38 +0100

    added imports

--- app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
+++ app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
@@ -1,11 +1,21 @@
 package it.reply.iriscube.unito
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import it.reply.iriscube.unito.activity.LoginActivity
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-/**
- * Created by Reply.
- */
 
+@RunWith(AndroidJUnit4::class)
 class LoginActivityTest {
 
 
@@ -14,6 +24,8 @@ class LoginActivityTest {
 
     @Test
     fun successfulLoginShouldOpenMainScreen() {
+
         //TODO: Check that the login is successful and that the WelcomeActivity intent starts correctly
+
     }
 }
