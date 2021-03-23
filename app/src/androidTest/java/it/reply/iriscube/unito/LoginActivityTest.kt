f8701f79b8c7c60019f89fe309fbf548672e170d
m.livraga <m.livraga@reply.it>
2021-03-23 16:33:01 +0100

    changed solution

--- app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
+++ app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
@@ -3,12 +3,12 @@ package it.reply.iriscube.unito
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.intent.Intents.intended
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
-import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtraWithKey
+import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import it.reply.iriscube.unito.activity.LoginActivity
+import it.reply.iriscube.unito.activity.WelcomeActivity
 import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
@@ -35,7 +35,7 @@ class LoginActivityTest {
 
         intended(
             allOf(
-                hasExtraWithKey(LoginActivity.USERNAME),
+                hasComponent(WelcomeActivity::class.java.name),
                 hasExtra(LoginActivity.USERNAME, username)
             )
         )
