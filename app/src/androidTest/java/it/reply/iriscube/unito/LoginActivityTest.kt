6de113c3aa41d2a70faa6612705d1215fc1dde1e
m.livraga <m.livraga@reply.it>
2021-03-27 17:00:11 +0100

    added descriptions

--- app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
+++ app/src/androidTest/java/it/reply/iriscube/unito/LoginActivityTest.kt
@@ -19,13 +19,20 @@ import org.junit.runner.RunWith
 class LoginActivityTest {
 
 
-    //TODO: Define the correct rule to check for the intent
+    // TODO Define the correct rule to check for the intent using IntentsTestRule
 
 
+    /**
+     * Check that the login is successful and that the WelcomeActivity intent starts correctly
+     */
     @Test
     fun successfulLoginShouldOpenMainScreen() {
 
-        //TODO: Check that the login is successful and that the WelcomeActivity intent starts correctly
+        // TODO (1) type a username and password inside the correct EdiText
+
+        // TODO (2) perform a click to login
+
+        // TODO (3) validate intent using intended() and IntentMatchers
 
     }
 }
