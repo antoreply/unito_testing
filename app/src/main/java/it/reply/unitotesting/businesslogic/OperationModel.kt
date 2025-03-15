d425e7a995bb4c7c6245ef26a676b82343da393b
antoniomaterazzo <a.materazzo@reply.it>
2025-03-15 13:37:01 +0100

    Refactored Operation model

--- app/src/main/java/it/reply/unitotesting/businesslogic/OperationModel.kt
+++ app/src/main/java/it/reply/unitotesting/businesslogic/OperationModel.kt
@@ -1,10 +1,42 @@
 package it.reply.unitotesting.businesslogic
 
-data class OperationModel(
-    val firstOperand: String? = null,
-    val secondOperand: String? = null,
-    val operator: Operator? = null
-)
+interface Operation{
+    fun getFirstOperand(): String?
+    fun getSecondOperand(): String?
+    fun getOperator(): Operator?
+}
+
+class OperationModel(): Operation{
+
+    private var firstOperand: String? = null
+
+    private var secondOperand: String? = null
+
+    private var operator: Operator? = null
+
+    constructor(firstOperand: String?, secondOperand: String?, operator: Operator?) : this() {
+        this.firstOperand = firstOperand
+        this.secondOperand = secondOperand
+        this.operator = operator
+    }
+
+    override fun getFirstOperand(): String? {
+        return firstOperand
+    }
+
+    override fun getSecondOperand(): String? {
+        return secondOperand
+    }
+
+    override fun getOperator(): Operator? {
+        return operator
+    }
+
+    fun getOperands(): List<String?>{
+        return listOf(firstOperand,secondOperand)
+    }
+
+}
 
 
 enum class Operator(val value:String){
