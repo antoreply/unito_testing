27fc5671516dacbf97ccf22ba7a848394ad1006d
antoniomaterazzo <a.materazzo@reply.it>
2025-03-15 13:38:43 +0100

    MainViewModel refactoring

--- app/src/main/java/it/reply/unitotesting/businesslogic/MainViewModel.kt
+++ app/src/main/java/it/reply/unitotesting/businesslogic/MainViewModel.kt
@@ -2,26 +2,32 @@ package it.reply.unitotesting.businesslogic
 
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-
-    private val _isAmountMenuExpanded = mutableStateOf(false)
-    val isAmountMenuExpanded: MutableState<Boolean> = _isAmountMenuExpanded
-
-    private val _result = mutableStateOf(ResultModel())
-    val result: MutableState<ResultModel> = _result
 
     // Funzione da svuotare e testare
     fun execute(operation: OperationModel){
 
     }
 
+    /**
+     *
+     *
+     *  Non modificare il codice da qui in basso
+     *
+     * **/
+
     /** Pass to this function the result you want to print **/
     private fun updateResult(resultModel: ResultModel){
         _result.value = resultModel
     }
+
+    private val _isAmountMenuExpanded = mutableStateOf(false)
+    val isAmountMenuExpanded: MutableState<Boolean> = _isAmountMenuExpanded
+
+    private val _result = mutableStateOf(ResultModel())
+    val result: MutableState<ResultModel> = _result
 
     companion object{
         const val DIV_ZERO_ERROR = "Non puoi dividere per 0"
